1268fab06a63d752b16220149b5a2e02e5f5d80a
Jani Hyytiäinen <janihy@gmail.com>
2021-02-01 20:56:51 +0200

    initial commit

---
+++ modules/matka.py
@@ -0,0 +1,38 @@
+"""
+    made by tuplis 2021
+"""
+
+import sopel.module
+import math
+import requests
+from bs4 import BeautifulSoup as BS
+
+BASEURL = 'https://www.etaisyys.com/etaisyys'
+
+
+@sopel.module.rule(r'^.matka (\w+) (\w+)(?: (\d+))?')
+@sopel.module.example('!matka Helsinki Riihimäki 100')
+def module(bot, trigger):
+    start = trigger.group(1)
+    end = trigger.group(2)
+    speed = float(trigger.group(3) or 80)
+
+    if not start or not end:
+        bot.reply('!matka lähde kohde')
+
+    try:
+        response = requests.get(f"{BASEURL}/{start}/{end}", verify=False)
+        dom = BS(response.text, 'html.parser')
+        map = int(dom.find(id='totaldistancekm').get('value'), 10)
+        road = int(dom.find(id='distance').get('value'), 10)
+
+        if map == 0 or road == 0:
+            bot.reply('Meiltä on nyt valitettavasti matkat päässeet loppumaan')
+            return
+
+        time = road / speed
+        hours = math.floor(time)
+        minutes = math.floor((time - hours) * 60)
+        bot.reply(f'{start.title()} - {end.title()}: {map} km linnuntietä, {road} km tietä pitkin ({hours} h {minutes} min nopeudella {speed:.0f} km/h)')
+    except Exception as e:
+        bot.reply(f'Eipä ollu, varmaan kirjotit jotain väärin')
