a934fc3296bf24d7fa47c195a50b8cdbb217c810
Jani Hyytiäinen <janihy@gmail.com>
2021-02-27 00:46:28 +0200

    spotify.py: proof of concept, working

--- modules/spotify.py
+++ modules/spotify.py
@@ -7,16 +7,19 @@ from __future__ import (unicode_literals, absolute_import, division, print_funct
 from sopel import db
 from sopel.module import commands, require_privmsg, rule
 from sopel.config.types import StaticSection, ValidatedAttribute
+from base64 import b64encode
 
 import requests
 import json
 
-SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize?response_type=code&client_id={client_id}&scope=user-read-playback-state&redirect_uri=https%3A%2F%2Fdsg.fi%2Fbot-callback%2F"
 SPOTIFY_TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token"
+SPOTIFY_NP_ENDPOINT = "https://api.spotify.com/v1/me/player"
 
 
 class SpotifySection(StaticSection):
-    api = ValidatedAttribute('api')
+    client_id = ValidatedAttribute('client_id')
+    client_secret = ValidatedAttribute('client_secret')
 
 
 def setup(bot):
@@ -29,34 +32,36 @@ def configure(config):
     config.spotify.configure_setting('client_secret', 'Enter Spotify client secret: ')
 
 
-class NoUserSetException(Exception):
-    pass
-
-
-class NoUserException(Exception):
-    pass
-
-
-class NoTracksException(Exception):
-    pass
-
-
-def format_song_output(user, action, artist, song, album):
-    return f'♫ {user} {action} {artist} - {song} ♫'
-
-
-def get_action(last_track):
-    if last_track:
-        return 'is listening to'
+def format_song_output(user, np):
+    artist = np.get('artist')
+    title = np.get('title')
+    uri = np.get('uri')
+    if np.get('currently_playing'):
+        mode = "is listening to"
     else:
-        return 'last listened to'
+        mode = "last listened to"
+    return f'♫ {user} {mode} {artist} - {title} ({uri}) ♫'
 
 
 def get_now_playing(spotify):
-    return "Rick Astley - Never Gonna Give You Up"
+    headers = {
+        'Authorization': f'Bearer {spotify.get("access_token")}'
+    }
+    res = requests.get(SPOTIFY_NP_ENDPOINT, headers=headers)
+    item = res.json().get('item')
+
+    np = {
+        'artist': item.get('artists', [{}])[0].get('name'),
+        'title': item.get('name'),
+        'uri': item.get('uri'),
+        'currently_playing': res.json().get('is_playing')
+    }
+
+    return np
 
 
-@commands('sdebug')
+@require_privmsg()
+@rule(r'^.spotify debug')
 def spotify_debug(bot, trigger):
     state = db.SopelDB(bot.config)
     spotify = state.get_nick_value(trigger.nick, 'spotify')
@@ -65,42 +70,52 @@ def spotify_debug(bot, trigger):
 
 @commands('snp')
 def spotify_np(bot, trigger):
-
+    client_id = bot.config.spotify.client_id
     state = db.SopelDB(bot.config)
     spotify = state.get_nick_value(trigger.nick, 'spotify')
     if not spotify:
         bot.reply('Laitoin sulle msg.')
-        bot.say('spotify piste com', trigger.nick)
+        bot.say(SPOTIFY_AUTH_ENDPOINT.format(client_id=client_id), trigger.nick)
+    else:
+        # the user has already registered and everything should be fine
+        np = get_now_playing(spotify)
+        bot.say(format_song_output(trigger.nick, np))
 
     # out = format_song_output(trigger.nick, action, last_track['artist']['#text'], last_track['name'], last_track['album']['#text'])
 
 
 @require_privmsg()
-@rule(r'^.spotify auth (\w+)')
+@rule(r'^.spotify auth (\S+)')
 def spotify_authenticate(bot, trigger):
     authcode = trigger.group(1)
+    client_id = bot.config.spotify.client_id
+    client_secret = bot.config.spotify.client_secret
+    state = db.SopelDB(bot.config)
 
     headers = {
-        'Authorization': authcode
+        'Authorization': 'Basic ' + b64encode(f'{client_id}:{client_secret}'.encode('utf-8')).decode('utf-8')
     }
     payload = {
         'grant_type': 'authorization_code',
-        'code': authcode
+        'code': authcode,
+        'redirect_uri': 'https://dsg.fi/bot-callback/'
     }
 
-    res = requests.post(SPOTIFY_AUTH_ENDPOINT, headers=headers, data=payload)
-    bot.reply(res.text)
+    res = requests.post(SPOTIFY_TOKEN_ENDPOINT, headers=headers, data=payload)
+    if res.status_code == 200:
+        response = json.loads(res.text)
 
-    state = db.SopelDB(bot.config)
+        spotify = {
+            'access_token': response.get('access_token'),
+            'expires_at': '',
+            'refresh_token': response.get('refresh_token')
+        }
+        state.set_nick_value(trigger.nick, 'spotify', spotify)
 
-    spotify = {
-        'access_token': '',
-        'expires_at': '',
-        'refresh_token': ''
-    }
-    state.set_nick_value(trigger.nick, 'spotify', spotify)
+        # ask spotify api for the access_token and the refresh_token, no need to really save the auth code
+        # that code will be expired quickly anyway
 
-    # ask spotify api for the access_token and the refresh_token, no need to really save the auth code
-    # that code will be expired quickly anyway
+        bot.reply('Spotify authentication key saved.')
 
-    bot.reply('Spotify authentication key saved.')
+    else:
+        bot.reply('Spotify authentication failed :-(')
