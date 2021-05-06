644435594a2d13e7402706bc82e3cf97617cb496
Jani Hyytiäinen <janihy@gmail.com>
2021-05-06 23:34:31 +0300

    stonk.py: add support for misspelled stonk names and make it more polite

--- modules/stonk.py
+++ modules/stonk.py
@@ -8,13 +8,36 @@ from sopel.formatting import color, colors
 from yfinance import Ticker
 
 
+def get_ticker(name: str) -> Ticker:
+    ticker = Ticker(name)
+    info = ticker.info
+    if not info.get("ask"):
+        if name[-3:] != ".HE":
+            if '.' in name:
+                # user probably wrote the name wrong
+                return get_ticker(f'{name.replace(".", "-")}')
+            # if not, maybe they forgot the helsinki börs (.he)
+            return get_ticker(f"{name}.HE")
+        return False
+    # print(ticker.dividends)
+
+    return ticker
+
+
 @commands('stonk')
 def trigger(bot, trigger):
     if not trigger.group(2):
-        bot.reply('sano vaikka !stonk tsla')
+        bot.reply('Kokeile !stonk tsla')
+        return False
+    name = trigger.group(2).upper()
+    ticker = get_ticker(name)
+    if not ticker:
+        bot.reply('Valitettavasti meiltä on päässeet numerot loppumaan :/')
         return False
     try:
-        info = Ticker(trigger.group(2)).info
+        info = ticker.info
+        if name != info.get("symbol", ""):
+            bot.reply(f"Semmosta ei oo, mutta kelpaako {info.get('symbol')}:")
         change = info.get('ask') - info.get('previousClose')
         changepercent = (change / info.get('previousClose')) * 100
         message = f'{info["shortName"]} ({info["symbol"].upper()}): {info["ask"]} {info["currency"]}'
@@ -22,7 +45,12 @@ def trigger(bot, trigger):
             message += color(f' {change:g} ({changepercent:.4f} %) ↑', colors.GREEN)
         else:
             message += color(f' {change:g} ({changepercent:.4f} %) ↓', colors.RED)
-    except Exception:
-        bot.reply('hyvä kysymys, kysyppä joku toinen')
+    except Exception as e:
+        bot.reply(f"{repr(e)} :(")
         return False
     bot.say(message)
+
+
+if __name__ == '__main__':
+    stonk = get_ticker('tsla')
+    print(stonk.info)
