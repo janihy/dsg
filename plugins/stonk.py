96de42e039adeb524dd2b67d71c6dfc36f7c7a1f
Jani Hyytiäinen <janihy@gmail.com>
2023-02-06 20:58:20 +0200

    upgrade yfinance because Yahoo changes, make faster and implement !superstonk

--- plugins/stonk.py
+++ plugins/stonk.py
@@ -1,50 +1,94 @@
 # coding=utf-8
 """
-    made by tuplis 2021
+    made by tuplis 2021-2023
 """
 
 from sopel.plugin import commands
 from sopel.formatting import color, colors
-from yfinance import Ticker
+import json
+import yfinance
 
 
-def get_ticker(name: str) -> Ticker:
-    ticker = Ticker(name)
-    info = ticker.info
-    if not info.get("ask"):
-        if name[-3:] != ".HE":
+def get_info(name: str, fast: bool = True) -> yfinance.scrapers.quote.InfoDictWrapper:
+    ticker_object = yfinance.Ticker(name)
+    try:
+        if fast:
+            info = ticker_object.fast_info
+            assert info.get("last_price")
+        else:
+            info = ticker_object.info
+            assert info.get("shortName")
+    except Exception:
+        if name[-3:].lower() != ".he":
             if '.' in name:
                 # user probably wrote the name wrong
-                return get_ticker(f'{name.replace(".", "-")}')
+                return get_info(f'{name.replace(".", "-")}', fast)
             # if not, maybe they forgot the helsinki börs (.he)
-            return get_ticker(f"{name}.HE")
-        return False
-    # print(ticker.dividends)
-
-    return ticker
+            return get_info(f"{name}.he", fast)
+        return None
+    return info
 
 
-@commands('stonk')
+def get_prices(ticker: str, period: str = "ytd") -> dict:
+    # see https://github.com/ranaroussi/yfinance/blob/main/yfinance/base.py#L467 for valid periods
+    # FIXME: move the .he suffix guess to somewhere usable in both here and get_info
+    ticker = yfinance.Ticker(ticker)
+    prices = ticker.history(period)
+    return prices
+
+
+@commands('stonk', 'superstonk')
 def trigger(bot, trigger):
     if not trigger.group(2):
-        bot.reply('Kokeile !stonk tsla')
+        bot.reply(f'Kokeile !{trigger.group(1)} TSLA')
         return False
-    name = trigger.group(2).upper()
-    ticker = get_ticker(name)
-    if not ticker:
+    ticker = trigger.group(2)
+    if trigger.group(1) == 'stonk':
+        fast = True
+    else:
+        fast = False
+    info = get_info(ticker, fast)
+
+    if not info:
         bot.reply('Valitettavasti meiltä on päässeet numerot loppumaan :/')
         return False
     try:
-        info = ticker.info
-        if name != info.get("symbol", ""):
-            bot.reply(f"Semmosta ei oo, mutta kelpaako {info.get('symbol')}:")
-        change = info.get('ask') - info.get('previousClose')
-        changepercent = (change / info.get('previousClose')) * 100
-        message = f'{info["shortName"]} ({info["symbol"].upper()}): {info["ask"]} {info["currency"]}'
-        if change > 0:
-            message += color(f' {change:g} ({changepercent:.4f} %) ↑', colors.GREEN)
+        if fast:
+            change = info.get('last_price') - info.get('previous_close')
+            changepercent = (change / info.get('previous_close'))
+            message = f'{ticker.upper()}: {info["last_price"]:.2f} {info["currency"]}'
+            prices = get_prices(ticker, "1d")
+            day_high = prices["High"].iloc[-1]
+            day_low = prices["Low"].iloc[-1]
+            if change > 0:
+                message += color(f' {change:+.2f} ({changepercent:+.4%}) {"🚀" if changepercent > 0.02 else "↑"}', colors.GREEN)
+            else:
+                message += color(f' {change:+.2f} ({changepercent:+.4%}) ↓', colors.RED)
+            message += f' Päivän ylin {day_high:.2f}, alin {day_low:.2f}'
         else:
-            message += color(f' {change:g} ({changepercent:.4f} %) ↓', colors.RED)
+            ytd_prices = get_prices(ticker, "ytd")
+            year_first = ytd_prices["Open"].iloc[0]
+            last_price = ytd_prices["Close"].iloc[-1]
+            message = f'{info.get("shortName")} ({ticker.upper()}): {last_price:.2f} {info["financialCurrency"]}.'
+
+            ytd = (last_price - year_first) / year_first
+            if ytd > 0:
+                indicator = colors.GREEN
+            else:
+                indicator = colors.RED
+            message += color(f' YTD {ytd:+.2%}.', indicator)
+
+            year_low = ytd_prices["Low"].min()
+            year_high = ytd_prices["High"].max()
+            message += f' Vuoden ylin {year_high:.2f}, alin {year_low:.2f}.'
+
+            pe = info.get("trailingPE")
+            if pe:
+                message += f' P/E {pe:.2f}.'
+
+            dividendyield = info.get("dividendYield") or None
+            if dividendyield:
+                message += f" Osinkotuotto {dividendyield:.2%}"
     except Exception as e:
         bot.reply(f"{repr(e)} :(")
         return False
@@ -52,5 +96,5 @@ def trigger(bot, trigger):
 
 
 if __name__ == '__main__':
-    stonk = get_ticker('tsla')
-    print(stonk.info)
+    # python3 stonk.py | jq --sort-keys .
+    print(json.dumps(dict(get_info("tsla", fast=False))))
