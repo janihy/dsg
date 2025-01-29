f7d4cf248a63fd296400d8b7a3a8df454568f70e
Jani Hyytiäinen <janihy@gmail.com>
2025-01-29 20:07:29 +0200

    fmi.py: refactor repetitive code, parse ww codes to human readable

--- plugins/fmi.py
+++ plugins/fmi.py
@@ -10,11 +10,14 @@ from bs4 import BeautifulSoup
 import pytz
 import requests
 import locale
+import json
 
 locale.setlocale(locale.LC_NUMERIC, "fi_FI.UTF-8")
 
-FMI_URL = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id={query_id}&place={place}&timezone=Europe/Helsinki&starttime={starttime}"
-WEATHERCODE_MAP = {
+FMI_URL = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id={query_id}&place={place}&timezone=Europe/Helsinki&starttime={starttime}&endtime={endtime}"
+
+# https://www.ilmatieteenlaitos.fi/latauspalvelun-pikaohje
+WEATHERSYMBOL3_MAP = {
     21: "heikkoja sadekuuroja",
     22: "sadekuuroja",
     23: "voimakkaita sadekuuroja",
@@ -43,6 +46,62 @@ WEATHERCODE_MAP = {
     2: "puolipilvistä",
     3: "pilvistä",
     -1: "palvelussa häire"
+}
+
+# https://www.ilmatieteenlaitos.fi/latauspalvelun-pikaohje
+WW_MAP = {
+    0: "ei merkittäviä sääilmiöitä (minkään alla olevan wawa-koodin ehdot eivät täyty)",
+    4: "auerta, savua tai ilmassa leijuvaa pölyä ja näkyvyys vähintään 1 km",
+    5: "auerta, savua tai ilmassa leijuvaa pölyä ja näkyvyys alle 1 km",
+    10: "utua",
+    20: "sumua",
+    21: "sadetta (olomuoto on määrittelemätön)",
+    22: "tihkusadetta (ei jäätävää) tai lumijyväsiä",
+    23: "vesisadetta (ei jäätävää)",
+    24: "lumisadetta",
+    25: "jäätävää vesisadetta tai jäätävää tihkua",
+    30: "sumua",
+    31: "sumua tai jääsumua erillisinä hattaroina",
+    32: "sumua tai jääsumua, joka on ohentunut edellisen tunnin aikana",
+    33: "sumua tai jääsumua, jonka tiheydessä ei ole tapahtunut merkittäviä muutoksia edellisen tunnin aikana",
+    34: "sumua tai jääsumua, joka on muodostunut tai tullut sakeammaksi edellisen tunnin aikana",
+    40: "sadetta",
+    41: "heikkoa tai kohtalaista sadetta (olomuoto on määrittelemätön)",
+    42: "kovaa sadetta (olomuoto on määrittelemätön)",
+    50: "tihkusadetta (heikkoa, ei jäätävää)",
+    51: "heikkoa tihkua, joka ei ole jäätävää",
+    52: "kohtalaista tihkua, joka ei ole jäätävää",
+    53: "kovaa tihkua, joka ei ole jäätävää",
+    54: "jäätävää heikkoa tihkua",
+    55: "jäätävää kohtalaista tihkua",
+    56: "jäätävää kovaa tihkua",
+    60: "vesisadetta (heikkoa, ei jäätävää)",
+    61: "heikkoa vesisadetta, joka ei ole jäätävää",
+    62: "kohtalaista vesisadetta, joka ei ole jäätävää",
+    63: "kovaa vesisadetta, joka ei ole jäätävää",
+    64: "jäätävää heikkoa vesisadetta",
+    65: "jäätävää kohtalaista vesisadetta",
+    66: "jäätävää kovaa vesisadetta",
+    67: "heikkoa lumensekaista vesisadetta tai tihkua (räntää)",
+    68: "kohtalaista tai kovaa lumensekaista vesisadetta tai tihkua (räntää)",
+    70: "lumisadetta",
+    71: "heikkoa lumisadetta",
+    72: "kohtalaista lumisadetta",
+    73: "tiheää lumisadetta",
+    74: "heikkoa jääjyvässadetta",
+    75: "kohtalaista jääjyväsadetta",
+    76: "kovaa jääjyväsadetta",
+    77: "lumijyväsiä",
+    78: "jääkiteitä",
+    80: "kuuroja tai ajoittaista sadetta(heikkoja)",
+    81: "heikkoja vesikuuroja",
+    82: "kohtalaisia vesikuuroja",
+    83: "kovia vesikuuroja",
+    84: "ankaria vesikuuroja (>32 mm/h)",
+    85: "heikkoja lumikuuroja",
+    86: "kohtalaisia lumikuuroja",
+    87: "kovia lumikuuroja",
+    89: "raekuuroja mahdollisesti yhdessä vesi- tai räntäsateen kanssa"
 }
 
 DIRECTION_MAP = {
@@ -88,56 +147,57 @@ def get_fmi_data(place: str) -> dict:
     # returned as a time value pair format.
     observations_query = "fmi::observations::weather::timevaluepair"
 
-    # The stored query can be used to fetch Harmonie surface level weather
-    # forecast in time value pair format. The model data covers the
-    # geographical area of Scandinavia. New forecast dataset will come
-    # available every 6 hours. Location need to be specified as place or geoid
-    # or latlon query parameters.
-    forecast_query = "fmi::forecast::harmonie::surface::point::timevaluepair"
+    # edited forecasts are curated by people inside FMI
+    # Edited Scandinavia point weather forecast fetched to a specific location
+    # returned in time value pair format. Location need to be specified as place
+    # or geoid or latlon query parameters.
+    forecast_query = "fmi::forecast::edited::weather::scandinavia::point::timevaluepair"
 
-    # starttime = (datetime.now(timezone.utc).replace(microsecond=0) - timedelta(hours=4)).isoformat()
-
-    # starttime = starttime.replace("+00:00", "Z")
-    starttime = int((datetime.now() - timedelta(hours=1)).timestamp())
+    starttime = (datetime.now() - timedelta(hours=1)).astimezone(pytz.utc).isoformat(timespec='seconds').replace("+00:00", "Z")
+    endtime = datetime.now().astimezone(pytz.utc).isoformat(timespec='seconds').replace("+00:00", "Z")
     result = {}
-    result['forecast'] = {}
+    forecast = {}
 
-    res = requests.get(FMI_URL.format(query_id=observations_query, place=place, starttime=starttime), timeout=5)
+    res = requests.get(FMI_URL.format(query_id=observations_query, place=place, starttime=starttime, endtime=endtime), timeout=5)
     try:
         res.raise_for_status()
         observations_soup = BeautifulSoup(res.text, features='lxml')
         result['timestamp'] = datetime.fromisoformat(observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-t2m'}).find_all('wml2:time')[-1].text)
         result['place'] = observations_soup.find('gml:name').text
-        result['temperature'] = convert_float(observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-t2m'}).find_all('wml2:value')[-1].text)
-        result['winddirection'] = observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-wd_10min'}).find_all('wml2:value')[-1].text
-        result['winddirection'] = convert_float(result['winddirection'])
-        result['windspeed'] = convert_float(observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-ws_10min'}).find_all('wml2:value')[-1].text)
-        result['rh'] = convert_float(observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-rh'}).find_all('wml2:value')[-1].text)
-        result['visibility'] = observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-vis'}).find_all('wml2:value')[-1].text
-        result['visibility'] = convert_float(result['visibility'])
-        result['clouds'] = observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-n_man'}).find_all('wml2:value')[-1].text
-        result['clouds'] = convert_float(result['clouds'])
-        result['rainfall'] = convert_float(observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-r_1h'}).find_all('wml2:value')[-1].text)
-        result['snow'] = convert_float(observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-snow_aws'}).find_all('wml2:value')[-1].text)
-        result['weather'] = round(convert_float(observations_soup.find('wml2:measurementtimeseries', {'gml:id': 'obs-obs-1-1-wawa'}).find_all('wml2:value')[-1].text))
+
+        observations_map = {
+            "temperature": "obs-obs-1-1-t2m",
+            "winddirection": "obs-obs-1-1-wd_10min",
+            "windspeed": "obs-obs-1-1-ws_10min",
+            "rh": "obs-obs-1-1-rh",
+            "visibility": "obs-obs-1-1-vis",
+            "clouds": "obs-obs-1-1-n_man",
+            "rainfall": "obs-obs-1-1-r_1h",
+            "snow": "obs-obs-1-1-snow_aws",
+            "weather": "obs-obs-1-1-wawa"
+        }
+        result = {k: convert_float(observations_soup.find('wml2:measurementtimeseries', {'gml:id': v}).find_all('wml2:value')[-1].text) for k, v in observations_map.items()}
+        result['weather'] = int(result['weather'])
     except Exception as e:
         print(e)
         return None
 
     try:
-        res = requests.get(FMI_URL.format(query_id=forecast_query, place=place, starttime=starttime), timeout=5)
+        timestamp = (datetime.now() + timedelta(days=1)).replace(hour=15, minute=0, second=0, microsecond=0)
+        timestamp = timestamp.isoformat().replace("+00:00", "Z")
+        res = requests.get(FMI_URL.format(query_id=forecast_query, place=place, starttime=timestamp, endtime=timestamp), timeout=5)
         res.raise_for_status()
+
         forecast_soup = BeautifulSoup(res.text, features='lxml')
-        # RIP WeatherSymbol3, does not exist in the harmonie forecast. Old Hirlam forecast was deprecated in 10/2022.
-        # result['weather'] = int(float(forecast_soup.find('wml2:measurementtimeseries', {'gml:id': 'mts-1-1-WeatherSymbol3'}).find_all('wml2:value')[0].text)) if not "NaN" else -1
-        # force it to -1 until we fix it
-        timestamp = (result['timestamp'] + timedelta(days=1)).replace(hour=15, minute=0, second=0, microsecond=0).astimezone(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
-        result['forecast']['temp'] = float(forecast_soup.find('wml2:measurementtimeseries', {'gml:id': 'mts-1-1-Temperature'}).find('wml2:time', string=timestamp).find_next_sibling('wml2:value').text)
-        result['forecast']['windspeed'] = float(forecast_soup.find('wml2:measurementtimeseries', {'gml:id': 'mts-1-1-WindSpeedMS'}).find_all('wml2:value')[-1].text)
-        result['forecast']['cloudcoverage'] = float(forecast_soup.find('wml2:measurementtimeseries', {'gml:id': 'mts-1-1-TotalCloudCover'}).find_all('wml2:value')[-1].text)
-        # result['forecastweather'] = int(float(forecast_soup.find('wml2:measurementtimeseries', {'gml:id': 'mts-1-1-WeatherSymbol3'}).find('wml2:time', string=timestamp).find_next_sibling('wml2:value').text)) if not "NaN" else -1
+        forecast_map = {
+            "temp": "mts-1-1-Temperature",
+            "windspeed": "mts-1-1-WindSpeedMS",
+            "weather": "mts-1-1-WeatherSymbol3"
+        }
+        result['forecast'] = {k: convert_float(forecast_soup.find('wml2:measurementtimeseries', {'gml:id': v}).find('wml2:value').text) for k, v in forecast_map.items()}
+        result['forecast']['weather'] = int(result['forecast']['weather'])
     except Exception as e:
-        result['error'] = e
+        result['error'] = repr(e)
     return result
 
 
@@ -168,7 +228,7 @@ def print_weather(bot, trigger):
     weather['timestamp'] = weather['timestamp'].strftime('%H:%M')
     weather['winddirection'] = round(weather['winddirection'])
     weather['visibility'] = round(weather['visibility'] / 1000)
-    weather['weather'] = round(weather['weather'])
+    weather['weather'] = WW_MAP[round(weather['weather'])]
     try:
         weather['windfrom'] = DIRECTION_MAP[int((weather['winddirection'] + 22.5) / 45) % 8]
     except Exception:
@@ -193,12 +253,10 @@ def print_weather(bot, trigger):
         msg += " Lumensyvyys {snow:.0f} cm.".format(**weather)
     forecast = weather['forecast']
     if "temp" in forecast:
-        # weather['forecastweather'] = WEATHERCODE_MAP[weather['forecastweather']]
+        weather['forecastweather'] = WEATHERSYMBOL3_MAP[weather['forecastweather']]
         forecast['temp'] = round(forecast['temp'])
         forecast['windspeed'] = round(forecast['windspeed'])
-        msg += " Huomispäiväksi luvattu {temp:n}°C, tuulee {windspeed:n} m/s.".format(**forecast)
-        if forecast['cloudcoverage'] > 80:
-            msg += " Pilvistä."
+        msg += " Huomispäiväksi luvattu {temp:n}°C, tuulee {windspeed:n} m/s - {forecastweather}.".format(**forecast)
 
     if "error" in weather:
         msg += f" Ei kaikkia tietoja, koska {weather['error']}"
@@ -206,4 +264,4 @@ def print_weather(bot, trigger):
 
 
 if __name__ == "__main__":
-    print(get_fmi_data("Espoo"))
+    print(json.dumps(get_fmi_data("Espoo"), indent=2, default=str))
